b744b3d40f814b72e42abbf1c54e1b5163d191ca
andiddy <anna7anna7anna81@gmail.com>
2025-04-01 20:12:23 +0400

    Add files via upload

---
+++ Laba4kg/Laba4kg/AppFor4Laba.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Laba4kg
+{
+    partial class AppFor4Laba
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxForStartPic = new System.Windows.Forms.PictureBox();
+            this.pictureBoxFor1Exercise = new System.Windows.Forms.PictureBox();
+            this.pictureBoxFor2Exercise = new System.Windows.Forms.PictureBox();
+            this.pictureBoxFor3Exercise = new System.Windows.Forms.PictureBox();
+            this.buttonForStartPic = new System.Windows.Forms.Button();
+            this.buttonForExercise1 = new System.Windows.Forms.Button();
+            this.buttonForExercise2 = new System.Windows.Forms.Button();
+            this.buttonForExercise3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForStartPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFor1Exercise)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFor2Exercise)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFor3Exercise)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxForStartPic
+            // 
+            this.pictureBoxForStartPic.Location = new System.Drawing.Point(410, 22);
+            this.pictureBoxForStartPic.Name = "pictureBoxForStartPic";
+            this.pictureBoxForStartPic.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxForStartPic.TabIndex = 0;
+            this.pictureBoxForStartPic.TabStop = false;
+            // 
+            // pictureBoxFor1Exercise
+            // 
+            this.pictureBoxFor1Exercise.Location = new System.Drawing.Point(142, 333);
+            this.pictureBoxFor1Exercise.Name = "pictureBoxFor1Exercise";
+            this.pictureBoxFor1Exercise.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxFor1Exercise.TabIndex = 1;
+            this.pictureBoxFor1Exercise.TabStop = false;
+            // 
+            // pictureBoxFor2Exercise
+            // 
+            this.pictureBoxFor2Exercise.Location = new System.Drawing.Point(410, 333);
+            this.pictureBoxFor2Exercise.Name = "pictureBoxFor2Exercise";
+            this.pictureBoxFor2Exercise.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxFor2Exercise.TabIndex = 2;
+            this.pictureBoxFor2Exercise.TabStop = false;
+            // 
+            // pictureBoxFor3Exercise
+            // 
+            this.pictureBoxFor3Exercise.Location = new System.Drawing.Point(678, 333);
+            this.pictureBoxFor3Exercise.Name = "pictureBoxFor3Exercise";
+            this.pictureBoxFor3Exercise.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxFor3Exercise.TabIndex = 3;
+            this.pictureBoxFor3Exercise.TabStop = false;
+            // 
+            // buttonForStartPic
+            // 
+            this.buttonForStartPic.Location = new System.Drawing.Point(410, 228);
+            this.buttonForStartPic.Name = "buttonForStartPic";
+            this.buttonForStartPic.Size = new System.Drawing.Size(200, 23);
+            this.buttonForStartPic.TabIndex = 4;
+            this.buttonForStartPic.Text = "показать";
+            this.buttonForStartPic.UseVisualStyleBackColor = true;
+            this.buttonForStartPic.Click += new System.EventHandler(this.buttonForStartPic_Click);
+            // 
+            // buttonForExercise1
+            // 
+            this.buttonForExercise1.Enabled = false;
+            this.buttonForExercise1.Location = new System.Drawing.Point(142, 539);
+            this.buttonForExercise1.Name = "buttonForExercise1";
+            this.buttonForExercise1.Size = new System.Drawing.Size(200, 23);
+            this.buttonForExercise1.TabIndex = 5;
+            this.buttonForExercise1.Text = "показать";
+            this.buttonForExercise1.UseVisualStyleBackColor = true;
+            this.buttonForExercise1.Click += new System.EventHandler(this.buttonForExercise1_Click);
+            // 
+            // buttonForExercise2
+            // 
+            this.buttonForExercise2.Enabled = false;
+            this.buttonForExercise2.Location = new System.Drawing.Point(410, 539);
+            this.buttonForExercise2.Name = "buttonForExercise2";
+            this.buttonForExercise2.Size = new System.Drawing.Size(200, 23);
+            this.buttonForExercise2.TabIndex = 6;
+            this.buttonForExercise2.Text = "показать";
+            this.buttonForExercise2.UseVisualStyleBackColor = true;
+            this.buttonForExercise2.Click += new System.EventHandler(this.buttonForExercise2_Click);
+            // 
+            // buttonForExercise3
+            // 
+            this.buttonForExercise3.Enabled = false;
+            this.buttonForExercise3.Location = new System.Drawing.Point(678, 539);
+            this.buttonForExercise3.Name = "buttonForExercise3";
+            this.buttonForExercise3.Size = new System.Drawing.Size(200, 23);
+            this.buttonForExercise3.TabIndex = 7;
+            this.buttonForExercise3.Text = "показать";
+            this.buttonForExercise3.UseVisualStyleBackColor = true;
+            this.buttonForExercise3.Click += new System.EventHandler(this.buttonForExercise3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(407, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "исходное изображение";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(139, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 60);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "задание 1.\r\nПроизвести загрузку изображения и перевод цветного изображения в\r\nотт" +
+    "енки серого.\r\n";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(407, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 60);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "задание 2.\r\nЛинейное контрастирование малоконтрастного изображения";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(675, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 105);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "задание 3.\r\nЛинейная масочная фильтрация, сглаживающая маска \r\n𝐴 = 0,𝐵 = 1/9,\r\n" +
+    "       (1 1 1)\r\nM = (1 1 1)\r\n       (1 1 1)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(543, 569);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(67, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(410, 572);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "минимальное значение";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(410, 598);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "максимальное значение";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(543, 595);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(67, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // AppFor4Laba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 657);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonForExercise3);
+            this.Controls.Add(this.buttonForExercise2);
+            this.Controls.Add(this.buttonForExercise1);
+            this.Controls.Add(this.buttonForStartPic);
+            this.Controls.Add(this.pictureBoxFor3Exercise);
+            this.Controls.Add(this.pictureBoxFor2Exercise);
+            this.Controls.Add(this.pictureBoxFor1Exercise);
+            this.Controls.Add(this.pictureBoxForStartPic);
+            this.Name = "AppFor4Laba";
+            this.Text = "AppFor4Laba";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForStartPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFor1Exercise)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFor2Exercise)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFor3Exercise)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxForStartPic;
+        private System.Windows.Forms.PictureBox pictureBoxFor1Exercise;
+        private System.Windows.Forms.PictureBox pictureBoxFor2Exercise;
+        private System.Windows.Forms.PictureBox pictureBoxFor3Exercise;
+        private System.Windows.Forms.Button buttonForStartPic;
+        private System.Windows.Forms.Button buttonForExercise1;
+        private System.Windows.Forms.Button buttonForExercise2;
+        private System.Windows.Forms.Button buttonForExercise3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ Laba4kg/Laba4kg/AppFor4Laba.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Laba4kg
+{
+    public partial class AppFor4Laba : Form
+    {
+        public AppFor4Laba()
+        {
+            InitializeComponent();
+        }
+        //может возникать ошибка из-за неправильно указанного пути. перепроверьте его
+        public static Bitmap inputImg = new Bitmap(@"E:\Contrast.jpg");
+        public static Bitmap first = new Bitmap(inputImg.Width, inputImg.Height);
+
+
+        private void buttonForStartPic_Click(object sender, EventArgs e)
+        {
+            pictureBoxForStartPic.Image = inputImg;
+            pictureBoxForStartPic.SizeMode = PictureBoxSizeMode.Zoom;
+            buttonForExercise1.Enabled = true;
+        }
+
+        private void buttonForExercise1_Click(object sender, EventArgs e)
+        {
+            int v;
+            for (int i = 1; i < inputImg.Width; i++)
+            {
+                for (int j = 1; j < inputImg.Height; j++)
+                {
+                    v = Convert.ToInt32(0.3 * inputImg.GetPixel(i, j).R + 0.59 * inputImg.GetPixel(i, j).G + 0.11 * inputImg.GetPixel(i, j).B);
+                    first.SetPixel(i, j, Color.FromArgb(v, v, v));
+                }
+            }
+            pictureBoxFor1Exercise.Image = first;
+            pictureBoxFor1Exercise.SizeMode = PictureBoxSizeMode.Zoom;
+            buttonForExercise2.Enabled = true;
+            buttonForExercise3.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+        }
+        private void buttonForExercise2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox2.Text) || string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Введите все значения", "Ошибка");
+                return;
+            }
+
+            int f_min = Int32.Parse(textBox1.Text);
+            int f_max = Int32.Parse(textBox2.Text);
+
+            Bitmap inputImg2 = new Bitmap(pictureBoxFor1Exercise.Image);
+            Bitmap resultImg = new Bitmap(inputImg2.Width, inputImg2.Height);
+            for (int i = 0; i < inputImg2.Width; i++)
+            {
+                for (int j = 0; j < inputImg2.Height; j++)
+                {
+                    Color pixelColor = inputImg2.GetPixel(i, j);
+                    int grayValue = (int)(0.3 * pixelColor.R + 0.59 * pixelColor.G + 0.11 * pixelColor.B);
+
+                    if (grayValue > f_min && grayValue < f_max)
+                    {
+                        // Если значение яркости в заданном диапазоне, устанавливаем белый пиксель
+                        resultImg.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                    }
+                    else
+                    {
+                        // Иначе устанавливаем черный пиксель
+                        resultImg.SetPixel(i, j, Color.FromArgb(255, 0, 0, 0));
+                    }
+                }
+            }
+
+            // Устанавливаем полученное изображение в pictureBox
+            pictureBoxFor2Exercise.Image = resultImg;
+            pictureBoxFor2Exercise.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void buttonForExercise3_Click(object sender, EventArgs e)
+        {
+            int[,] M = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+            Bitmap inputImg2 = new Bitmap(pictureBoxFor1Exercise.Image);
+            Bitmap third = new Bitmap(inputImg2.Width, inputImg2.Height);
+            ProcessMainPixels(inputImg2, third, M);
+            ProcessBorderPixels(inputImg2, third, M);
+            pictureBoxFor3Exercise.Image = third;
+            pictureBoxFor3Exercise.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+        private void ProcessMainPixels(Bitmap input, Bitmap output, int[,] M)
+        {
+            for (int i = 2; i < input.Width - 1; i++)
+            {
+                for (int j = 2; j < input.Height - 1; j++)
+                {
+                    Color color = GetBrightness(input, M, i, j);
+                    output.SetPixel(i, j, color);
+                }
+            }
+        }
+        private void ProcessBorderPixels(Bitmap input, Bitmap output, int[,] M)
+        {
+            for (int j = 1; j < input.Height - 1; j++)
+            {
+
+                Color leftBorderColor = input.GetPixel(0, j);
+                output.SetPixel(0, j, leftBorderColor);
+
+                Color rightBorderColor = GetBrightness(input, M, input.Width - 1, j);
+                output.SetPixel(input.Width - 1, j, rightBorderColor);
+            }
+
+            
+            for (int i = 1; i < input.Width - 1; i++)
+            {
+                Color topBorderColor = input.GetPixel(i, 0);
+                output.SetPixel(i, 0, topBorderColor); 
+
+                Color bottomBorderColor = GetBrightness(input, M, i, input.Height - 1);
+                output.SetPixel(i, input.Height - 1, bottomBorderColor);
+            }
+        }
+        private Color GetBrightness(Bitmap input, int[,] M, int x, int y)
+        {
+            double brightnessR = 0, brightnessG = 0, brightnessB = 0;
+            double A = 0;
+            double B = 1.0 / 9.0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int offsetX = x - 1 + i;
+                    int offsetY = y - 1 + j;
+                    if (offsetX < 0 || offsetY < 0 || offsetX >= input.Width || offsetY >= input.Height) continue;
+
+                    Color pixelColor = input.GetPixel(offsetX, offsetY);
+                    brightnessR += M[i, j] * pixelColor.R;
+                    brightnessG += M[i, j] * pixelColor.G;
+                    brightnessB += M[i, j] * pixelColor.B;
+                }
+            }
+            int bR = Convert.ToInt32((brightnessR * B) + A);
+            int bG = Convert.ToInt32((brightnessB * B) + A);
+            int bB = Convert.ToInt32((brightnessG * B) + A);
+            return Color.FromArgb(Clamp(bR), Clamp(bG), Clamp(bB));
+        }
+        private int Clamp(int value)
+        {
+            return Math.Max(0, Math.Min(255, value));
+        }
+        public static Bitmap LinearContrast(Bitmap inputImg2, int threshold)
+        {
+            int width = inputImg2.Width;
+            int height = inputImg2.Height;
+            Bitmap outputImg = new Bitmap(width, height);
+            int fmin = 255;
+            int fmax = 0;
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Color pixelColor = inputImg2.GetPixel(x, y);
+                    int gray = pixelColor.R;
+
+                    if (gray < fmin)
+                        fmin = gray;
+                    if (gray > fmax)
+                        fmax = gray;
+                }
+            }
+
+            //если изображение состоит из пикселей одного цвета
+            if (fmin == fmax)
+            {
+                return (Bitmap)inputImg2.Clone();
+            }
+
+            int gmin = 0;
+            int gmax = 255;
+
+            float a = (float)(gmax - gmin) / (fmax - fmin);
+            float b = gmin - a * fmin;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Color pixelColor = inputImg2.GetPixel(x, y);
+                    int gray = pixelColor.R;
+
+                    int newGray;
+                    if (gray < threshold)
+                    {
+                        newGray = gmin;
+                    }
+                    else
+                    {
+                        newGray = (int)(a * gray + b);
+                        newGray = Math.Max(0, Math.Min(255, newGray));
+                    }
+                    outputImg.SetPixel(x, y, Color.FromArgb(newGray, newGray, newGray));
+                }
+            }
+
+            return outputImg;
+        }
+    }
+}
